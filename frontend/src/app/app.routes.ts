ccd80df6f81764bd806c330ec6d429e4af024bd5
Tobias <tobias.monyk@hotmail.com>
2026-03-29 16:22:49 +0200

    - Added profile to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import {ToursComponent} from './components/tours/tours';
 import {HomeComponent} from './components/home/home';
 import {LoginComponent} from './components/login/login';
 import {RegisterComponent} from './components/register/register';
+import {ProfileComponent} from './components/profile/profile';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'profile', component: ProfileComponent},
   {path: 'home', component: HomeComponent},
   {path: 'tours', component: ToursComponent},
   {path: 'login', component: LoginComponent},
