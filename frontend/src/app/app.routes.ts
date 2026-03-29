7a962d0e774c4f2ef2a1156f37918582683107e8
Tobias <tobias@monyk.at>
2026-03-29 11:43:29 +0200

    Created and linked in routes: HomeComponent, ToursComponent, LoginComponent, RegisterComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {ToursComponent} from './components/tours/tours';
+import {HomeComponent} from './components/home/home';
+import {LoginComponent} from './components/login/login';
+import {RegisterComponent} from './components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'map', component: ToursComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
